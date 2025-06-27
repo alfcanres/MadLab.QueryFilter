1e6c52d87a00c4c1c714186a9afebddc247581e5
Alfredo Can <alfcanres@gmail.com>
2025-06-27 14:13:40 -0500

    Refactor console app to use service layer for posts
    Modified `Program.cs` to utilize `PostService` for fetching posts, enhancing separation of concerns.

--- MadLab.QueryFilter.Demo.Console/Program.cs
+++ MadLab.QueryFilter.Demo.Console/Program.cs
@@ -1,7 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using MadLab.QueryFilter.Domain;
-using Microsoft.EntityFrameworkCore;
+
+using MadLab.QueryFilter.Domain.Repository;
+using MadLab.QueryFilter.Services.Services;
 
 
 string connectionString = "Data Source=SimpleBlog.db";
@@ -10,31 +12,30 @@ using (var context = new DataBaseContext(connectionString))
 {
     await DbInitializer.CreateDemoData(context);
 
-    var posts = await context.Posts
-        .Include(p => p.Author)
-        .Include(p => p.PostType)
-        .Include(p => p.MoodType)
-        .OrderByDescending(p => p.CreationDate)
-        .ToListAsync();
+    // Set up repository and service
+    var postRepository = new RepositoryBase<Post>(context);
+    var postService = new PostService(postRepository);
+
+    // Use the service to get all posts paged (first page, 100 posts)
+    var posts = await postService.GetAllPaged(1, 100);
 
     // Table header
     Console.WriteLine(new string('=', 120));
     Console.WriteLine(
-        $"| # | {"ID",3} | {"Title",-25} | {"Author",-15} | {"Type",-12} | {"Mood",-10} | {"Created",-16} | {"Pub",-3} | {"Content Preview",-25} |");
+        $"| # | {"ID",3} | {"Title",-25} | {"Author",-15} | {"Type",-12} | {"Mood",-10} | {"Created",-16} | {"Pub",-3} |");
     Console.WriteLine(new string('=', 120));
     int number = 1;
     // Table rows
     foreach (var post in posts)
     {
-        var authorName = post.Author?.UserName ?? "Unknown";
-        var postType = post.PostType?.Description ?? "N/A";
-        var moodType = post.MoodType?.Mood ?? "N/A";
-        var preview = post.Text?.Length > 25 ? post.Text.Substring(0, 22) + "..." : post.Text;
+        var authorName = post.Author ?? "Unknown";
+        var postType = post.PostTypeName ?? "N/A";
+        var moodType = post.MoodTypeName ?? "N/A";
         var created = post.CreationDate.ToString("yyyy-MM-dd HH:mm");
         var published = post.IsPublished ? "Yes" : "No";
 
         Console.WriteLine(
-            $"| {number} | {post.Id,3} | {Truncate(post.Title, 25),-25} | {Truncate(authorName, 15),-15} | {Truncate(postType, 12),-12} | {Truncate(moodType, 10),-10} | {created,-16} | {published,3} | {Truncate(preview, 25),-25} |");
+            $"| {number} | {post.Id,3} | {Truncate(post.Title, 25),-25} | {Truncate(authorName, 15),-15} | {Truncate(postType, 12),-12} | {Truncate(moodType, 10),-10} | {created,-16} | {published,3} |");
 
         number++;
     }
@@ -50,5 +51,3 @@ static string Truncate(string value, int maxLength)
     if (string.IsNullOrEmpty(value)) return "";
     return value.Length <= maxLength ? value : value.Substring(0, maxLength - 3) + "...";
 }
-
-
